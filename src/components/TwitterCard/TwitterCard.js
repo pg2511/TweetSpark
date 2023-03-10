15e0dabf8f2618ed1084ca78eb785d97c7343c6b
Prafful Gupta <praffulgupta09@gmail.com>
2023-03-10 10:47:43 +0530

    Twitter Card Component UI 🌟

--- src/components/TwitterCard/TwitterCard.js
+++ src/components/TwitterCard/TwitterCard.js
@@ -1,9 +1,42 @@
 import React from "react";
+import { Copy, Plus, X } from "react-feather";
 
 import styles from './TwitterCard.module.css'
 
 function TwitterCard() {
-    return <div className={styles.container}>TwitterCard</div>
+    return (
+        <div className={styles.container}>
+            <div className={`icon ${styles.close}`}>
+                <X/>
+            </div>
+            
+            <p className={styles.text}>This is my text in the card</p>
+            
+            <span className={styles.line} />
+
+            <div className={styles.bottom}>
+                <div className="icon">
+                    <Copy />
+                </div>
+
+                <div className={styles.right}>
+                    <div className={styles.progress} style={{ "--deg": "180deg" }}>
+                        <div className={styles.inner}/>
+                    </div>
+
+                    <span className={styles.bar} />
+
+                    <div className="icon">
+                        <Plus />
+                    </div>
+
+                    <span className={styles.bar}/>
+
+                    <button className="button">Generate Thread</button>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default TwitterCard;
