e7efc1cd7d301cc0a4b468c81606a176f64fc832
Prafful Gupta <praffulgupta09@gmail.com>
2023-03-12 13:10:57 +0530

    Generate Tweets 🐦

--- src/components/TwitterCard/TwitterCard.js
+++ src/components/TwitterCard/TwitterCard.js
@@ -1,42 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import { Copy, Plus, X } from "react-feather";
 
-import styles from './TwitterCard.module.css'
+import styles from "./TwitterCard.module.css";
 
-function TwitterCard() {
-    return (
-        <div className={styles.container}>
-            <div className={`icon ${styles.close}`}>
-                <X/>
-            </div>
-            
-            <p className={styles.text}>This is my text in the card</p>
-            
-            <span className={styles.line} />
+function TwitterCard({
+  tweet = "",
+  onTweetChange,
+  onClose,
+  showGenerateThreadBtn = false,
+  onAddClick,
+  placeholder = "",
+  onGenerateThreadClick,
+  generateButtonDisabled = false,
+}) {
+  const [isEditing, setIsEditing] = useState(false);
 
-            <div className={styles.bottom}>
-                <div className="icon">
-                    <Copy />
-                </div>
+  const handleCopy = async (text) => {
+    if (window.navigator?.clipboard?.writeText)
+      await window.navigator.clipboard.writeText(text);
+  };
 
-                <div className={styles.right}>
-                    <div className={styles.progress} style={{ "--deg": "180deg" }}>
-                        <div className={styles.inner}/>
-                    </div>
+  const progress = (tweet.length / 280) * 100;
+  const progressDeg = parseInt((progress * 360) / 100);
+  const charactersLeft = 280 - tweet.length;
 
-                    <span className={styles.bar} />
+  return (
+    <div className={styles.container}>
+      <div
+        className={`icon ${styles.close}`}
+        onClick={() => (onClose ? onClose() : "")}
+      >
+        <X />
+      </div>
+      {isEditing ? (
+        <textarea
+          defaultValue={tweet}
+          placeholder={placeholder || "write here"}
+          onChange={(event) =>
+            onTweetChange ? onTweetChange(event.target.value) : ""
+          }
+          onBlur={() => setIsEditing(false)}
+          autoFocus
+        />
+      ) : !tweet.length && placeholder ? (
+        <p
+          className={styles.text}
+          style={{ color: "gray", fontWeight: "400" }}
+          onClick={() => setIsEditing(true)}
+        >
+          {placeholder}
+        </p>
+      ) : (
+        <p className={styles.text} onClick={() => setIsEditing(true)}>
+          {tweet}
+        </p>
+      )}
 
-                    <div className="icon">
-                        <Plus />
-                    </div>
+      <span className={styles.line} />
 
-                    <span className={styles.bar}/>
-
-                    <button className="button">Generate Thread</button>
-                </div>
-            </div>
+      <div className={styles.bottom}>
+        <div className="icon" onClick={() => handleCopy(tweet)}>
+          <Copy />
         </div>
-    );
+
+        <div className={styles.right}>
+          <div
+            className={styles.progress}
+            style={{
+              "--deg": `${progressDeg}deg`,
+              "--color": charactersLeft < 0 ? "red" : "#55acee",
+            }}
+          >
+            <div
+              className={`${styles.inner} ${
+                charactersLeft < 0 ? styles.red : ""
+              }`}
+            >
+              {charactersLeft < 60 ? charactersLeft : ""}
+            </div>
+          </div>
+
+          <span className={styles.bar} />
+
+          <div
+            className="icon"
+            onClick={() => (onAddClick ? onAddClick() : "")}
+          >
+            <Plus />
+          </div>
+
+          {showGenerateThreadBtn ? (
+            <>
+              <span className={styles.bar} />
+              <button
+                className="button"
+                onClick={() =>
+                  onGenerateThreadClick ? onGenerateThreadClick() : ""
+                }
+                disabled={generateButtonDisabled}
+              >
+                {generateButtonDisabled ? "Generating..." : "Generate Thread"}
+              </button>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TwitterCard;
--- src/components/TwitterPage/TwitterPage.js
+++ src/components/TwitterPage/TwitterPage.js
@@ -1,122 +1,213 @@
-import React, { useEffect, useState } from 'react'
-import { Copy } from 'react-feather'
-import { generatePrompts } from '../../apis/twitter';
-import TwitterCard from '../TwitterCard/TwitterCard'
+import React, { useEffect, useState } from "react";
+import { Copy } from "react-feather";
 
-import styles from './TwitterPage.module.css'
+import TwitterCard from "../TwitterCard/TwitterCard";
 
-function TwitterPage () {
+import {
+  generatePrompts,
+  generateThread,
+} from "../../apis/twitter";
 
-    const [prompts, setPrompts] = useState([]);
-    const [promptInput, setPromptInput] = useState("");
-    const [errorMessages, setErrorMessages] = useState({
-        prompt: "",
-        thread: "",
-        image: "",
+import styles from "./TwitterPage.module.css";
+
+function TwitterPage() {
+  const [prompts, setPrompts] = useState([]);
+  const [promptInput, setPromptInput] = useState("");
+  const [errorMessages, setErrorMessages] = useState({
+    prompt: "",
+    thread: "",
+    image: "",
+  });
+  const [disabledButtons, setDisabledButtons] = useState({
+    prompt: false,
+    thread: false,
+    image: false,
+  });
+  const [thread, setThread] = useState([]);
+  const [imageUrl, setImageUrl] = useState("");
+
+  const generateUniqueId = () =>
+    (Math.random() * 98975958 + Date.now()).toString(16);
+
+  const handleCopy = async (text) => {
+    if (window.navigator?.clipboard?.writeText)
+      await window.navigator.clipboard.writeText(text);
+  };
+
+  const handleThreadDeletion = (index) => {
+    const tempThread = [...thread];
+
+    tempThread.splice(index, 1);
+    setThread(tempThread);
+  };
+
+  const handleThreadAddition = (index) => {
+    const tempThread = [...thread];
+
+    tempThread.splice(index + 1, 0, {
+      id: generateUniqueId(),
+      placeholder: "Write here...",
+      value: "",
     });
+    setThread(tempThread);
+  };
 
-    // disabling buttons while api is calling
-    const [disabledButtons, setDisabledButtons] = useState({
-        prompt: false,
-        thread: false,
-        image: false,
-    });
+  const handleThreadChange = (val, id) => {
+    const tempThread = [...thread];
+
+    const index = tempThread.findIndex((item) => item.id === id);
+    if (index < 0) return;
+
+    tempThread[index].value = val;
+    setThread(tempThread);
+  };
 
 
-    const handleCopy = async (text) => {
-        if (window.navigator?.clipboard?.writeText)
-          await window.navigator.clipboard.writeText(text);
-    };
+  const handleThreadGeneration = async () => {
+    if (thread.length == 0 || disabledButtons.thread) return;
 
-      
-    const handlePromptGeneration = async () => {
-        if(!promptInput.trim()) {
-            setErrorMessages((prev) => ({
-                ...prev, 
-                prompt: "Please enter something"
-            }));
-            return;
-        }
+    const tempThread = [...thread];
+    const lastTweet = tempThread.pop().value;
 
-        setErrorMessages((prev) => ({
-            ...prev, 
-            prompt: ""
-        }));
+    setDisabledButtons((prev) => ({ ...prev, thread: true }));
+    const res = await generateThread(lastTweet);
+    setDisabledButtons((prev) => ({ ...prev, thread: false }));
 
-        setPrompts([]);
-        setDisabledButtons((prev) => ({ ...prev, prompt: true }));
-        const res = await generatePrompts(promptInput);
-        setDisabledButtons((prev) => ({ ...prev, prompt: false }));
+    if (!res) return;
 
-        if(!res) {
-            setErrorMessages((prev) => ({
-                ...prev, 
-                prompt: "Error getting the prompts, please try again"
-            }));
-            return;
-        }
+    const data = res.choices[0].text;
+    const newThread = data
+      .split("\n")
+      .filter((item) => item.trim())
+      .map((item) => item.slice(2).trim());
 
-        const tweets = res.choices.map((item) => item.text.trim());
-        setPrompts(tweets);
+    const finalThread = newThread.map((item) => ({
+      id: generateUniqueId(),
+      placeholder: "Write here",
+      value: item,
+    }));
+    setThread((prev) => [...prev, ...finalThread]);
+  };
+
+  const handlePromptGeneration = async () => {
+    if (!promptInput.trim()) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        prompt: "Please enter something",
+      }));
+      return;
+    }
+    setErrorMessages((prev) => ({
+      ...prev,
+      prompt: "",
+    }));
+
+    setPrompts([]);
+    setDisabledButtons((prev) => ({ ...prev, prompt: true }));
+    const res = await generatePrompts(promptInput);
+    setDisabledButtons((prev) => ({ ...prev, prompt: false }));
+    if (!res) {
+      setErrorMessages((prev) => ({
+        ...prev,
+        prompt: "Error getting the prompts, please try again",
+      }));
+      return;
     }
 
-    useEffect(() => {}, []);
+    const tweets = res.choices.map((item) => item.text.trim());
+    setPrompts(tweets);
+  };
 
-    return (
-        <div className={styles.container}>
+  useEffect(() => {}, []);
 
-            <div className={styles.mainLeft}>
-                <div className={styles.topSection}>
-                    <label>What's in your mind today ?</label>
-                    <textarea 
-                    placeholder='Write here...'
-                    value={promptInput}
-                    onChange={event=>setPromptInput(event.target.value)}
-                    />
-                    <button 
-                        className='button' 
-                        onClick={handlePromptGeneration}
-                        disabled={disabledButtons.prompt}
-                    >
-                        {disabledButtons.prompt ? "Generating..." : "Generate prompts"}
-                    </button>
-                </div>
-
-                <span className={styles.line} />
-
-                <div className={styles.prompts}>
-                    <p className={styles.heading}>Suggested prompts</p>
-                    {disabledButtons.prompt ? (
-                        <p>Loading...</p>
-                    ) : prompts.length == 0 ? (
-                        <p>No prompts to show, generate some new prompts. </p>
-                    ) : (
-                    prompts.map((item) => (
-                        <div className={styles.prompt} key={item}>
-                            <p className={styles.text}>{item}</p>
-                            <div className={styles.bottom}> 
-                                <div className='icon' onClick={() => handleCopy(item)}>
-                                    <Copy />
-                                </div>
-                            </div>
-                        </div>
-                        ))
-                    )}
-                </div>
-            </div>
-
-            <div className={styles.mainRight}>
-                <p className={styles.heading}>Your tweet</p>
-                <p className={styles.btn}>+Write a tweet</p>
-
-                <div className={styles.cards}>
-                    <TwitterCard />
-                    <TwitterCard />
-                    <TwitterCard />
-                </div>
-            </div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.mainLeft}>
+        <div className={styles.topSection}>
+          <label>What's in your mind today ?</label>
+          <textarea
+            placeholder="Writer here..."
+            value={promptInput}
+            onChange={(event) => setPromptInput(event.target.value)}
+          />
+          {errorMessages.prompt ? (
+            <p className="error">{errorMessages.prompt}</p>
+          ) : (
+            ""
+          )}
+          <button
+            className="button"
+            onClick={handlePromptGeneration}
+            disabled={disabledButtons.prompt}
+          >
+            {disabledButtons.prompt ? "Generating..." : "Generate prompts"}
+          </button>
         </div>
-    )
+
+        <span className={styles.line} />
+
+        <div className={styles.prompts}>
+          <p className={styles.heading}>Suggested prompts</p>
+          {disabledButtons.prompt ? (
+            <p>Loading...</p>
+          ) : prompts.length == 0 ? (
+            <p>No prompts to show, generate some new prompts. </p>
+          ) : (
+            prompts.map((item) => (
+              <div className={styles.prompt} key={item}>
+                <p className={styles.text}>{item}</p>
+                <div className={styles.bottom}>
+                  <div className="icon" onClick={() => handleCopy(item)}>
+                    <Copy />
+                  </div>
+                </div>
+              </div>
+            ))
+          )}
+        </div>
+      </div>
+      <div className={styles.mainRight}>
+        <p className={styles.heading}>Your tweets</p>
+
+        
+
+        {thread.length == 0 ? (
+          <p
+            className={styles.btn}
+            onClick={() =>
+              setThread([
+                {
+                  id: generateUniqueId(),
+                  value: "",
+                  placeholder: "Write here...",
+                },
+              ])
+            }
+          >
+            +Write a tweet
+          </p>
+        ) : (
+          ""
+        )}
+
+        <div className={styles.cards}>
+          {thread.map((item, index) => (
+            <TwitterCard
+              key={item.id}
+              placeholder={item.placeholder}
+              tweet={item.value}
+              onTweetChange={(val) => handleThreadChange(val, item.id)}
+              showGenerateThreadBtn={index === thread.length - 1}
+              onAddClick={() => handleThreadAddition(index)}
+              onClose={() => handleThreadDeletion(index)}
+              onGenerateThreadClick={handleThreadGeneration}
+              generateButtonDisabled={disabledButtons.thread}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default TwitterPage
+export default TwitterPage;
